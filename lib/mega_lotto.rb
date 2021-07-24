cb55ec15425f1ad6232e0f0c976d0a72afbe8afb
Nana Adjei Manu <claeusdev@gmail.com>
2021-07-24 23:57:26 +0000

    add specs and core functionality

--- lib/mega_lotto.rb
+++ lib/mega_lotto.rb
@@ -5,4 +5,17 @@ require_relative "mega_lotto/version"
 module MegaLotto
   class Error < StandardError; end
   # Your code goes here...
+
+  # Simple class top generate the lotto numbers
+  class Drawing
+    def draw
+      5.times.map { single_draw }
+    end
+
+    private
+
+    def single_draw
+      rand(1..100)
+    end
+  end
 end
--- spec/mega_lotto_spec.rb
+++ spec/mega_lotto_spec.rb
@@ -5,7 +5,27 @@ RSpec.describe MegaLotto do
     expect(MegaLotto::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(true).to eq(true)
+  describe "#draw" do
+    let(:drawing) { MegaLotto::Drawing.new.draw }
+    it "returns an array" do
+      expect(drawing).to be_a(Array)
+    end
+
+    it "returns an array with 5 elements" do
+      expect(drawing.size).to eq(5)
+    end
+
+    it "each element is an integer" do
+      drawing.each do |item|
+        expect(item).to be_a(Integer)
+      end
+    end
+
+    it "each element is 0 < 100" do
+      drawing.each do |item|
+        expect(item).to be > 0
+        expect(item).to be < 100
+      end
+    end
   end
 end
